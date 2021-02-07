77dad4dbc5eb483a8172e57931be567c59f4d7ba
SteveEarl-AWS <smearl+github@amazon.com>
2021-02-07 11:19:12 -0500

    Initial Commit

--- Solutions/AWSSCV-DashboardAlarms/Code/awsscv_alarm_to_chatter/awsscv_alarm_to_chatter.py
+++ Solutions/AWSSCV-DashboardAlarms/Code/awsscv_alarm_to_chatter/awsscv_alarm_to_chatter.py
@@ -1,9 +1,63 @@
 from awsscv.sf import Salesforce
 
-def lambda_handler(event, context):
-    response = {}
+logger = logging.getLogger()
 
-    # Handle EventBridge pings that keep the function warm
-    if 'source' in event:
-        response.update({'statusCode': 200,'response' : 'warm', 'event' : 'EventBridge ping'})
-        return response
+def lambda_handler(event, context):
+    sf = Salesforce()
+
+    try:
+        for record in event['Records']:
+            results = sf.create_formatted_chatter_post('0F94W000000pThySAE', format_record(record), 'AllUsers');
+
+        return { "Success": True, "Id": results['id'] }
+
+    except Exception as err:
+        return { "Success": False, "Error": str(err) }
+
+def format_record(record):
+    messageSegments = []
+
+    messageSegments.extend([
+        { 'markupType': 'Paragraph', 'type': 'MarkupBegin' },
+        { 'markupType': 'Bold', 'type': 'MarkupBegin' },
+        { 'text': record['Sns']['Subject'], 'type': 'Text'},
+        { 'markupType': 'Bold', 'type': 'MarkupEnd' },
+        { 'markupType': 'Paragraph', 'type': 'MarkupEnd' }
+    ])
+
+    message = json.loads(record['Sns']['Message'])
+
+    trigger = message['Trigger']
+
+    del message['Trigger']
+
+    for key in message:
+        value = message[key]
+
+        print('Processing: {} - {}'.format(key, value))
+        messageSegments.extend([
+            { 'markupType': 'Paragraph', 'type': 'MarkupBegin' },
+            { 'text': '{} - {}'.format(key, value), 'type': 'Text' },
+            { 'markupType': 'Paragraph', 'type': 'MarkupEnd' }
+        ])
+
+    messageSegments.extend([
+        { 'text': 'Trigger', 'type': 'Text'},
+        { 'markupType': 'UnorderedList', 'type': 'MarkupBegin' }
+    ])
+
+    for key in trigger:
+        value = trigger[key]
+
+        messageSegments.extend([
+            { 'markupType': 'ListItem', 'type': 'MarkupBegin' },
+            { 'text': '{} - {}'.format(key, value), 'type': 'Text' },
+            { 'markupType': 'ListItem', 'type': 'MarkupEnd' }
+        ])
+
+
+    messageSegments.extend([
+        { 'markupType': 'UnorderedList', 'type': 'MarkupEnd' }
+    ])
+
+    return messageSegments
