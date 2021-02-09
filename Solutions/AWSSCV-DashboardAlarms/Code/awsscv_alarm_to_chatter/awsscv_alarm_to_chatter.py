d0aebf7f7e3e6f53653d8496b11ca66d0d62e5ef
SteveEarl-AWS <smearl+github@amazon.com>
2021-02-09 16:22:17 -0500

    Updated handling of simple SNS messages
    Updated CF template with SalesforceChatterFeedId parameter

--- Solutions/AWSSCV-DashboardAlarms/Code/awsscv_alarm_to_chatter/awsscv_alarm_to_chatter.py
+++ Solutions/AWSSCV-DashboardAlarms/Code/awsscv_alarm_to_chatter/awsscv_alarm_to_chatter.py
@@ -7,16 +7,23 @@ logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
 def lambda_handler(event, context):
     logger.debug(event)
 
-    sf = Salesforce()
-
     try:
-        for record in event['Records']:
-            results = sf.create_formatted_chatter_post('0F94W000000pThySAE', format_record(record), 'AllUsers');
+        sf_chatter_feed_id = os.getenv('sf_chatter_feed_id', None)
 
-        return { "success": True, "id": results['id'] }
+        ids = []
+
+        if sf_chatter_feed_id is not None:
+            sf = Salesforce()
+
+            for record in event['Records']:
+                results = sf.create_formatted_chatter_post(sf_chatter_feed_id, format_record(record), 'AllUsers');
+                logger.debug(results)
+                ids.append({ "id": results['id'] })
+
+        return { "success": True, "ids": ids }
 
     except Exception as e:
-        logger.err(e)
+        logger.error(e)
         return { "success": False, "error": str(e) }
 
 def format_record(record):
@@ -30,39 +37,49 @@ def format_record(record):
         { 'markupType': 'Paragraph', 'type': 'MarkupEnd' }
     ])
 
-    message = json.loads(record['Sns']['Message'])
+    try:
+        message = json.loads(record['Sns']['Message'])
 
-    trigger = message['Trigger']
+        trigger = message['Trigger']
 
-    del message['Trigger']
+        del message['Trigger']
 
-    for key in message:
-        value = message[key]
+        for key in message:
+            value = message[key]
+
+            messageSegments.extend([
+                { 'markupType': 'Paragraph', 'type': 'MarkupBegin' },
+                { 'text': '{} - {}'.format(key, value), 'type': 'Text' },
+                { 'markupType': 'Paragraph', 'type': 'MarkupEnd' }
+            ])
+
+        messageSegments.extend([
+            { 'text': 'Trigger', 'type': 'Text'},
+            { 'markupType': 'UnorderedList', 'type': 'MarkupBegin' }
+        ])
+
+        for key in trigger:
+            value = trigger[key]
+
+            messageSegments.extend([
+                { 'markupType': 'ListItem', 'type': 'MarkupBegin' },
+                { 'text': '{} - {}'.format(key, value), 'type': 'Text' },
+                { 'markupType': 'ListItem', 'type': 'MarkupEnd' }
+            ])
+
+
+        messageSegments.extend([
+            { 'markupType': 'UnorderedList', 'type': 'MarkupEnd' }
+        ])
+
+    except Exception as e:
+        message = record['Sns']['Message']
 
         messageSegments.extend([
             { 'markupType': 'Paragraph', 'type': 'MarkupBegin' },
-            { 'text': '{} - {}'.format(key, value), 'type': 'Text' },
+            { 'text': 'Message - {}'.format(message), 'type': 'Text' },
             { 'markupType': 'Paragraph', 'type': 'MarkupEnd' }
         ])
-
-    messageSegments.extend([
-        { 'text': 'Trigger', 'type': 'Text'},
-        { 'markupType': 'UnorderedList', 'type': 'MarkupBegin' }
-    ])
-
-    for key in trigger:
-        value = trigger[key]
-
-        messageSegments.extend([
-            { 'markupType': 'ListItem', 'type': 'MarkupBegin' },
-            { 'text': '{} - {}'.format(key, value), 'type': 'Text' },
-            { 'markupType': 'ListItem', 'type': 'MarkupEnd' }
-        ])
-
-
-    messageSegments.extend([
-        { 'markupType': 'UnorderedList', 'type': 'MarkupEnd' }
-    ])
 
     logger.debug(messageSegments)
 
