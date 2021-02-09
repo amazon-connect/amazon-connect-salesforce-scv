dae9115bf1eaac81f39234b9c327b8a7ba11deba
SteveEarl-AWS <smearl+github@amazon.com>
2021-02-09 18:36:29 -0500

    Removed AWSSCV-DashboardAlarms prior to merge to main

--- Solutions/AWSSCV-DashboardAlarms/Code/awsscv_alarm_to_log/awsscv_alarm_to_log.py
+++
@@ -1,7 +0,0 @@
-import os, json, logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
-
-def lambda_handler(event, context):
-    logger.info(event)
--- Solutions/AWSSCV-DashboardAlarms/readme.md
+++
@@ -1,2 +0,0 @@
-# AWSSCV Dashboard Alarms
-AWSSCV Dashboard Alarms provides a set of Cloudwatch alarms allowing administrators to receive notifications of alarms for various Amazon Connect metrics.
