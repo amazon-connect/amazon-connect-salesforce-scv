f64902c1735c168eae299c2e9928ee1135186c05
SteveEarl-AWS <smearl+github@amazon.com>
2021-02-10 08:39:05 -0500

    Moved validator Lambda to external resource rather than inline

--- Common/AWSSCV-SalesforceConfig/Code/awsscv_salesforce_validator/awsscv_salesforce_validator.py
+++ Common/AWSSCV-SalesforceConfig/Code/awsscv_salesforce_validator/awsscv_salesforce_validator.py
@@ -1,36 +1,39 @@
 """
 **********************************************************************************************************************
- *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved                                            *
- *                                                                                                                    *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated      *
- *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation   *
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and  *
- *  to permit persons to whom the Software is furnished to do so.                                                     *
- *                                                                                                                    *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO  *
- *  THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF         *
- *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS *
- *  IN THE SOFTWARE.                                                                                                  *
- **********************************************************************************************************************
+*  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved                                            *
+*                                                                                                                    *
+*  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated      *
+*  documentation files (the "Software"), to deal in the Software without restriction, including without limitation   *
+*  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and  *
+*  to permit persons to whom the Software is furnished to do so.                                                     *
+*                                                                                                                    *
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO  *
+*  THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF         *
+*  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS *
+*  IN THE SOFTWARE.                                                                                                  *
+**********************************************************************************************************************
 """
 
-# Import the necessary moduels for this flow to work
+import os, logging
 from awsscv.sf import Salesforce
 
+logger = logging.getLogger()
+logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
+
 def lambda_handler(event, context):
+    logger.debug(event)
 
     try:
         sf = Salesforce()
         qr = sf.query("SELECT Id, Username FROM User LIMIT 1")
 
         return {
-            "success": True,
-            "message": "Salesforce connectivity validation was successful."
+            "Status": "SUCCESS"
         }
 
     except Exception as e:
         return {
-            "success": False,
-            "message": "Salesforce connectivity validation failed. {}".format(str(e))
+            "Status": "FAILURE",
+            "Reason": str(e)
         }
