799c0931c185a54e1abe5bcd65b825bc8bbc96c0
SteveEarl-AWS <smearl+github@amazon.com>
2021-03-28 11:34:04 -0400

    Initial Commit

---
+++ Examples/AWSSCV-SampleContactFlows/Code/awsscv_sample_contact_flow_builder/awsscv_sample_contact_flow_builder.py
@@ -0,0 +1,82 @@
+import sys, json, os, logging
+from awsscv.cfb import ContactFlowBuilder
+
+flowPath = os.environ['LAMBDA_TASK_ROOT'] + '/awsscv_ddr_flow_cf.json'
+
+logger = logging.getLogger()
+logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
+
+config = {
+    "DDRFlow": {
+        "Name": "SomeTestFlow",
+        "Type": "CONTACT_FLOW",
+        "Description": "Some test flow",
+        "Source": os.environ['LAMBDA_TASK_ROOT'] + '/awsscv_ddr_flow_cf.json',
+        "SubMap": {
+        }
+    }
+}
+
+
+def lambda_handler(event, context):
+    logger.debug(event)
+
+    cfb = ContactFlowBuilder()
+
+    try:
+        for (k, v) in config.items():
+            file_source = open(v['Source']).read()
+            flow_content = json.loads(file_source)['ContactFlow']['Content']
+
+            result = cfb.create_contact_flow(
+                event['Parameters']['ConnectInstanceId'],
+                v['Name'],
+                v['Type'],
+                v['Description'],
+                flow_content,
+                v['Tags'],
+                v['SubMap']
+            )
+
+            cf_send(event, context, 'SUCCESS', {})
+            return result;
+
+    except Exception as e:
+        logger.error(e)
+        cf_send(event, context, 'FAILED', {})
+        return e
+
+
+def cf_send(event, context, status, data, physical_resource_id=None, no_echo=False):
+    response_url = event['ResponseURL']
+
+    logger.debug(response_url)
+
+    response = {
+        'Status': status,
+        'Reason': 'See the details in CloudWatch Log Stream: ' + context.log_stream_name,
+        'PhysicalResourceId': physical_resource_id or context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'NoEcho': no_echo,
+        'Data': data
+    }
+
+    json_response = json.dumps(response)
+
+    logger.debug("Response body:\n{}".format(json_response))
+
+    headers = {
+        'content-type': '',
+        'content-length': str(len(json_response))
+    }
+
+    try:
+
+        response = http.request('PUT', response_url, body=json_response.encode('utf-8'), headers=headers)
+        logger.debug("Status code: {0}".format(response.reason))
+
+    except Exception as e:
+        logger.error(e)
+        logger.debug("send(..) failed executing requests.put(..): {}".format(str(e)))
