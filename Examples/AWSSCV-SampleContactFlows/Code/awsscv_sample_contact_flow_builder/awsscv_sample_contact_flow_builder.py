0aef4fec414a2ce655146d6447f5868ef2b3329c
SteveEarl-AWS <smearl+github@amazon.com>
2021-04-05 14:25:57 -0400

    Correct lambda function spelling mistake

--- Examples/AWSSCV-SampleContactFlows/Code/awsscv_sample_contact_flow_builder/awsscv_sample_contact_flow_builder.py
+++ Examples/AWSSCV-SampleContactFlows/Code/awsscv_sample_contact_flow_builder/awsscv_sample_contact_flow_builder.py
@@ -4,7 +4,7 @@ from pip._internal import main
 main(['install', 'boto3', '--target', '/tmp/'])
 sys.path.insert(0, '/tmp/')
 
-import os, json, logging, boto3, urllib3
+import os, json, logging, boto3, urllib3, cfnresponse
 
 logger = logging.getLogger()
 logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
@@ -14,6 +14,14 @@ http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
     logger.debug(event)
+
+    if event['RequestType'] == 'Delete':
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+
+        return {
+            'result': 'success',
+            'event': 'Delete'
+        }
 
     task_root = os.environ['LAMBDA_TASK_ROOT']
 
@@ -43,19 +51,19 @@ def lambda_handler(event, context):
 
             results.append(result)
 
-        cf_send(event, context, 'SUCCESS', {})
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
         return results
 
     except Exception as e:
         logger.error(e)
-        cf_send(event, context, 'FAILED', {})
+        cfnresponse.send(event, context, cfnresponse.FAILED, {})
 
 
 def create_contact_flow(connect_instance_id, name, type, description, content, sub_map):
     sub_content = content
 
     for key, value in sub_map.items():
-        sub_content = sub_content.replace(key, value);
+        sub_content = sub_content.replace(key, value)
 
     result = boto3.client('connect').create_contact_flow(
         InstanceId=connect_instance_id,
@@ -66,33 +74,3 @@ def create_contact_flow(connect_instance_id, name, type, description, content, s
     )
 
     return result
-
-
-def cf_send(event, context, status, data, physical_resource_id=None, no_echo=False):
-    response_url = event['ResponseURL']
-
-    response = {
-        'Status': status,
-        'Reason': 'See the details in CloudWatch Log Stream: ' + context.log_stream_name,
-        'PhysicalResourceId': physical_resource_id or context.log_stream_name,
-        'StackId': event['StackId'],
-        'RequestId': event['RequestId'],
-        'LogicalResourceId': event['LogicalResourceId'],
-        'NoEcho': no_echo,
-        'Data': data
-    }
-
-    json_response = json.dumps(response)
-
-    headers = {
-        'content-type': '',
-        'content-length': str(len(json_response))
-    }
-
-    try:
-        response = http.request('PUT', response_url, body=json_response.encode('utf-8'), headers=headers)
-        logger.debug("Status code: {0}".format(response.reason))
-
-    except Exception as e:
-        logger.error(e)
-        logger.debug("send(..) failed executing http.request(..): {}".format(str(e)))
