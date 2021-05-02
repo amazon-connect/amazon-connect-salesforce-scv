a704ea343ee9ea1077e834b7e64be81b686364dd
Steve Earl <smearl@amazon.com>
2021-05-02 17:10:21 -0400

    Updated contact flow contents

--- Examples/AWSSCV-SampleContactFlows/Code/awsscv_sample_contact_flow_builder/awsscv_sample_contact_flow_builder.py
+++ Examples/AWSSCV-SampleContactFlows/Code/awsscv_sample_contact_flow_builder/awsscv_sample_contact_flow_builder.py
@@ -4,13 +4,16 @@ from pip._internal import main
 main(['install', 'boto3', '--target', '/tmp/'])
 sys.path.insert(0, '/tmp/')
 
-import os, json, logging, boto3, urllib3, cfnresponse
+import os, json, logging, boto3, urllib3, calendar, time
 
 logger = logging.getLogger()
-logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
+logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'DEBUG')))
 
 http = urllib3.PoolManager()
 
+sub_map = {}
+contact_flow_map = {}
+results = []
 
 def lambda_handler(event, context):
     logger.debug(event)
@@ -26,52 +29,95 @@ def lambda_handler(event, context):
 
         task_root = os.environ['LAMBDA_TASK_ROOT']
 
-        config_source = open(task_root + '/config.json').read()
-        config_content = json.loads(config_source)
+        sub_map['%%CONNECT_BASIC_QUEUE_ARN%%'] = os.getenv('connect_basic_queue_arn')
+        sub_map['%%INVOKE_TELEPHONY_FUNCTION_ARN%%'] = os.getenv('invoke_telephony_function_arn')
+        sub_map['%%INVOKE_SALESFORCE_REST_API_FUNCTION_ARN%%'] = os.getenv('invoke_salesforce_rest_api_function_arn')
+        sub_map['%%KVS_CONSUMER_TRIGGER_ARN%%'] = os.getenv('kvs_consumer_trigger_arn')
 
-        results = []
+        ts = calendar.timegm(time.gmtime())
 
-        for (k, v) in config_content.items():
-            file_source = open(task_root + '/' + v['Source']).read()
-            flow_content = json.loads(file_source)['ContactFlow']['Content']
+        # Process AWS_Sample_SCV_Agent_Transfer
+        with open(os.path.join(task_root, 'AWS_Sample_SCV_Agent_Transfer.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
 
-            sub_map = v['SubMap']
-            sub_map['%%CONNECT_BASIC_QUEUE_ARN%%'] = os.getenv('connect_basic_queue_arn')
-            sub_map['%%INVOKE_TELEPHONY_FUNCTION_ARN%%'] = os.getenv('invoke_telephony_function_arn')
-            sub_map['%%INVOKE_SALESFORCE_REST_API_FUNCTION_ARN%%'] = os.getenv('invoke_salesforce_rest_api_function_arn')
-            sub_map['%%KVS_CONSUMER_TRIGGER_ARN%%'] = os.getenv('kvs_consumer_trigger_arn')
+        # Process AWSSCVAgentWhisper
+        with open(os.path.join(task_root, 'AWSSCVAgentWhisper.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
 
-            result = create_contact_flow(
-                os.getenv('connect_instance_id'),
-                v['Name'],
-                v['Type'],
-                v['Description'],
-                flow_content,
-                sub_map
-            )
+        # Process AWSSCVAgentWhisperWithStreaming
+        with open(os.path.join(task_root, 'AWSSCVAgentWhisperWithStreaming.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
 
-            results.append(result)
+        # Process AWS_Sample_SCV_Queue_Transfer
+        with open(os.path.join(task_root, 'AWS_Sample_SCV_Queue_Transfer.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
+
+        # Process AWS_Sample_SCV_REST_Example
+        with open(os.path.join(task_root, 'AWS_Sample_SCV_REST_Example.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
+
+        # Process AWS_Sample_SCV_Inbound
+        with open(os.path.join(task_root, 'AWS_Sample_SCV_Inbound.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
+
+        # Process AWS_Sample_SCV_Inbound_Flow_with_Transcription
+        with open(os.path.join(task_root, 'AWS_Sample_SCV_Inbound_Flow_with_Transcription.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
+
+        # Process AWS_Sample_SCV_Outbound_Flow_with_Transcription
+        with open(os.path.join(task_root, 'AWS_Sample_SCV_Outbound_Flow_with_Transcription.json')) as f:
+            json_object = json.load(f)
+            result = create_contact_flow(os.getenv('connect_instance_id'), json_object, ts)
+        time.sleep(2)
+
 
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
-        return results
+
+        return contact_flow_map
 
     except Exception as e:
         logger.error(e)
-        cfnresponse.send(event, context, cfnresponse.FAILED, {})
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
 
 
-def create_contact_flow(connect_instance_id, name, type, description, content, sub_map):
-    sub_content = content
+def create_contact_flow(connect_instance_id, json_object, ts):
+    name = json_object['ContactFlow']['Name']
+    type = json_object['ContactFlow']['Type']
+    description = json_object['ContactFlow']['Description']
+    sub_content = json_object['ContactFlow']['Content']
 
     for key, value in sub_map.items():
         sub_content = sub_content.replace(key, value)
 
-    result = boto3.client('connect').create_contact_flow(
-        InstanceId=connect_instance_id,
-        Name=name,
-        Type=type.upper(),
-        Description=description,
-        Content=sub_content
-    )
+    try:
+        result = boto3.client('connect').create_contact_flow(
+            InstanceId=connect_instance_id,
+            Name=name + '-' + str(ts),
+            Type=type,
+            Description=description,
+            Content=sub_content
+        )
 
-    return result
+        sub_map['%%' + name + '%%'] = result['ContactFlowArn']
+
+        contact_flow_map[name] = { 'ContactFlowId': result['ContactFlowId'], 'ContactFlowArn': result['ContactFlowArn'] }
+
+        return result
+
+    except Exception as e:
+        logger.error(e)
+        logger.error(sub_content)
