793fba1b1b8e42dea3ea81956947e10e4faf90ea
Steve Earl <smearl@amazon.com>
2021-06-01 16:49:05 -0400

    Updates

--- Solutions/AWSSCV-AgentEventLogger/Code/awsscv_ae_logger/awsscv_ae_logger.py
+++ Solutions/AWSSCV-AgentEventLogger/Code/awsscv_ae_logger/awsscv_ae_logger.py
@@ -1,9 +1,6 @@
-import os
-import json
-import boto3
-import logging
-import base64
+import os, json, boto3, logging, base64
 
+detailed_logging = os.getenv('detailed_logging', 'false')
 format = os.getenv('format', 'true')
 
 logger = logging.getLogger()
@@ -14,9 +11,23 @@ def lambda_handler(event, context):
 
     for record in event['Records']:
         payload = json.loads(base64.b64decode(record['kinesis']['data']))
+        event_type = payload['EventType']
 
-        if (payload['EventType'] != 'HEART_BEAT'):
+        try:
+            previousAgentStatusARN = payload['PreviousAgentSnapshot']['AgentStatus']['ARN']
+        except Error as e:
+            previousAgentStatusARN = '0'
+
+        try:
+            currentAgentStatusARN = payload['CurrentAgentSnapshot']['AgentStatus']['ARN']
+        except Error as e:
+            currentAgentStatusARNM = '1'
+
+        if (detailed_logging == 'true'):
             log_to_console(payload)
+        else:
+            if ((event_type == 'STATE_CHANGE' and currentAgentStatusARN != previousAgentStatusARN) or (event_type == 'LOGIN' or event_type == 'LOGOUT')):
+                log_to_console(payload)
 
 def log_to_console(record):
     logger.info(json.dumps(record, skipkeys = True, indent = 2) if format == 'true' else json.dumps(record))
