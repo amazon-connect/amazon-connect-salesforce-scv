90a30c4d72c82f7c974419cfc4780fc691767fc1
gandchin <gandchin@amazon.co.uk>
2022-04-05 11:57:41 +0100

    added code

---
+++ Examples/AWSSCV-ContactLens-PostCallAnalysis/code/lambda_function.py
@@ -0,0 +1,83 @@
+# This lambda function extracts information such as matched call categories and overall (agent & customer) sentiment, post call from Connect Contact Lens.
+# It also inserts the information in corresponding fields in Voice Call Object of Salesforce. 
+# User can easily extend this lambda function to extract and insert more information generate by Contact Lens such as call transcript, conversation characteristics etc.
+
+
+import json
+import boto3
+import random
+import os
+import logging
+
+
+s3 = boto3.resource('s3')
+lambda_client = boto3.client('lambda')
+
+logger = logging.getLogger()
+logger.setLevel(logging.getLevelName(os.getenv('lambda_logging_level', 'INFO')))
+
+def lambda_handler(event, context):
+     
+    # This lambda function will be triggered once the Contact Lens post call analysis in available in the associated S3 bucket
+     
+     logger.info(event)
+     logger.info(event["Records"][0]["s3"]["bucket"]["name"])
+     logger.info(event["Records"][0]["s3"]["object"]["key"])
+     
+     bucket = event["Records"][0]["s3"]["bucket"]["name"]
+     key = event["Records"][0]["s3"]["object"]["key"]
+     key = key.replace('%3A', ':')
+
+     
+     obj = s3.Object(bucket,key)
+     clObject = json.loads(obj.get()['Body'].read().decode('utf-8'))
+     logger.info(clObject)
+    
+     
+     ContactId = clObject['CustomerMetadata']['ContactId']
+     InstanceId = clObject['CustomerMetadata']['InstanceId']
+     logger.info("Contact ID: " + clObject['CustomerMetadata']['ContactId'])
+     logger.info(clObject['CustomerMetadata']['InstanceId'])
+     
+     # Extracting information from Contact Lens post call analysis
+     
+     Overall_Agent_Sentiment = clObject['ConversationCharacteristics']['Sentiment']['OverallSentiment']['AGENT']
+     Overall_Customer_Sentiment = clObject['ConversationCharacteristics']['Sentiment']['OverallSentiment']['CUSTOMER']
+     
+     Categories = ""
+     for x in range (len(clObject['Categories']['MatchedCategories'])):
+        Categories = Categories + clObject['Categories']['MatchedCategories'][x] + ","
+     Categories = Categories[:len(Categories)-1]
+     
+    # Setting up data to insert into corresponding fields in Salesforce Voice Call Object. 
+    # Please ensure you have Salesforce Voice Call Object created before invoking this lambda function 
+    # In this example - Call_Categories, Overall_Customer, Overall_Agent_Sentiment (__c will be automatically added by Salesforce)
+
+     
+     cldata = {}
+     cldata['Call_Categories__c'] = Categories
+     cldata['Overall_Customer_Sentiment__c'] = Overall_Customer_Sentiment
+     cldata['Overall_Agent_Sentiment__c'] = Overall_Agent_Sentiment
+     logger.info(Categories)
+     logger.info(Overall_Customer_Sentiment)
+     logger.info(Overall_Agent_Sentiment)
+     
+     fields = {
+        "callAttributes": json.dumps(cldata)
+     }
+     payload = {
+         "Details": {
+           "Parameters" : {
+              "methodName": "updateVoiceCall",
+              "fieldValues": fields,
+               "contactId": ContactId
+           }
+         }
+     }
+     
+    # This lambda function uses "updateVoiceCall" method of TelephonyIntegrationApiFunction lambda function which comes out-of-the-box with Service Cloud Voice
+     
+     response = lambda_client.invoke(FunctionName = os.environ['ARN_InvokeTelephonyIntegrationApiFunction'], InvocationType = 'RequestResponse', Payload = json.dumps(payload))
+     logger.info(json.loads(response["Payload"].read())['status'])
+     return response["StatusCode"]
+    
