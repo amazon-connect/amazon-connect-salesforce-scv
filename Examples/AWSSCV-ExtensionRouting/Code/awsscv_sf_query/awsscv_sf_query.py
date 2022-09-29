59c04d0426aaa4c8aa445722483031f6a7a4b298
Diego C <dcarbonetti@gmail.com>
2022-09-30 08:57:58 +1000

    Update awsscv_sf_query.py
    Changed query to only look for active users

--- Examples/AWSSCV-ExtensionRouting/Code/awsscv_sf_query/awsscv_sf_query.py
+++ Examples/AWSSCV-ExtensionRouting/Code/awsscv_sf_query/awsscv_sf_query.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
     try:
         sf_sso_object = event['Details']['Parameters']['sf_sso_object']
         sf_extension = event['Details']['Parameters']['sf_extension']
-        sf_query = "SELECT Id, " + sf_sso_object + " FROM User WHERE Extension ='" + sf_extension + "'"
+        sf_query = "SELECT Id, " + sf_sso_object + " FROM User WHERE IsActive = TRUE AND Extension ='" + sf_extension + "'"
 
         # Login to Salesforce
         try:
