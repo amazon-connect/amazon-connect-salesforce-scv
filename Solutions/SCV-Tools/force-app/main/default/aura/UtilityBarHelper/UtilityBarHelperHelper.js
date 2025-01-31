01ee1724a8b35771443d8aead15bcb39ef09c385
Steve Earl <69601384+SteveEarl-AWS@users.noreply.github.com>
2025-01-31 12:16:42 -0500

    Delete Solutions/SCV-Tools directory

--- Solutions/SCV-Tools/force-app/main/default/aura/UtilityBarHelper/UtilityBarHelperHelper.js
+++
@@ -1,8 +0,0 @@
-({
-    postMessageToChild: function (cmp, data) {
-        const vfOrigin = "https://" + cmp.get("v.vfHost");
-        const vfWindow = cmp.find("utiltyBarHelperFrame").getElement().contentWindow;
-        data["source"] = "utilityBarHelper";
-        vfWindow.postMessage(data, vfOrigin);
-    }
-});
